06547ca483ff7459df8ca7c8c108e33a48010795
hitchman007 <120267158+hitchman007@users.noreply.github.com>
2026-07-07 23:27:40 -0400

    Create PlayerData.cs

---
+++ Backend/Scripts/Database/PlayerData.cs
@@ -0,0 +1,15 @@
+using System;
+
+[Serializable]
+public class PlayerData
+{
+    public string playerID;
+
+    public string username;
+
+    public int level;
+
+    public int experience;
+
+    public string avatarID;
+}
