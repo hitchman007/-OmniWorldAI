e1e076b495003a689c5d8315241660bf304a6908
hitchman007 <120267158+hitchman007@users.noreply.github.com>
2026-07-07 23:28:17 -0400

    Create WorldData.cs

---
+++ Backend/Scripts/Database/WorldData.cs
@@ -0,0 +1,15 @@
+using System;
+
+[Serializable]
+public class WorldData
+{
+    public string worldID;
+
+    public string creatorID;
+
+    public string worldName;
+
+    public string description;
+
+    public bool published;
+}
