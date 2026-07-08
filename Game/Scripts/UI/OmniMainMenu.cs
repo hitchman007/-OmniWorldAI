6706c21aa9a1002e65ccc6389a841ffa6eb53c52
hitchman007 <120267158+hitchman007@users.noreply.github.com>
2026-07-07 23:20:02 -0400

    Create OmniMainMenu.cs

---
+++ Game/Scripts/UI/OmniMainMenu.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class OmniMainMenu : MonoBehaviour
+{
+    public void EnterWorld()
+    {
+        Debug.Log(
+        "Entrando a NeoGenesis"
+        );
+    }
+
+    public void CreateWorld()
+    {
+        Debug.Log(
+        "Abriendo OmniCreate"
+        );
+    }
+
+    public void OpenGuide()
+    {
+        Debug.Log(
+        "Abriendo OmniGuide"
+        );
+    }
+}
